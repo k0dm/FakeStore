9fd77481679502bd56f2d164e6c3c3c3ca8dc9b2
deizerok <pavlo.kiperchuk@gmail.com>
2024-02-24 21:04:17 +0200

    FS-04 Screen implementation and use livedata in Activity

--- app/src/main/java/com/example/fakestore/main/Screen.kt
+++ app/src/main/java/com/example/fakestore/main/Screen.kt
@@ -1,6 +1,34 @@
 package com.example.fakestore.main
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
 interface Screen {
 
-    object Empty : Screen
+    fun show(id: Int, supportFragmentManager: FragmentManager)
+
+
+    object Empty : Screen {
+        override fun show(id: Int, supportFragmentManager: FragmentManager) = Unit
+    }
+
+    abstract class Replace(private val clasz: Class<out Fragment>) : Screen {
+        override fun show(id: Int, supportFragmentManager: FragmentManager) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(id, clasz, null)
+                .commit()
+        }
+    }
+
+    abstract class Add(private val clasz: Class<out Fragment>) : Screen {
+        override fun show(id: Int, supportFragmentManager: FragmentManager) {
+            supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(id, clasz, null)
+                .addToBackStack(clasz.simpleName)
+                .commit()
+        }
+    }
+
 }
--- app/src/main/java/com/example/fakestore/products/CategoryScreen.kt
+++ app/src/main/java/com/example/fakestore/products/CategoryScreen.kt
@@ -1,5 +1,10 @@
 package com.example.fakestore.products
 
+import androidx.fragment.app.FragmentManager
 import com.example.fakestore.main.Screen
 
-object CategoryScreen : Screen
+object CategoryScreen : Screen {
+    override fun show(id: Int, supportFragmentManager: FragmentManager) {
+        TODO("Not yet implemented")
+    }
+}
